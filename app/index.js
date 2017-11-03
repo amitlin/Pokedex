84cccc428c15e72abaef2b84f03751204b56f974
Amit Lin <amitlin.dev@gmail.com>
2017-11-03 23:18:02 +0200

    github pages troubleshooting
    for the life of me i dont understand what im doing wrong

---
+++ app/index.js
@@ -0,0 +1,59 @@
+var Promise = require("bluebird");
+const Vue = require("vue");
+const axios = require("axios");
+const _ = require("lodash");
+
+
+// components
+const pokeType = require('./components/pokeType/component');
+
+
+let app = new Vue({
+  el: "#app",
+  components: {
+    pokeType
+  },
+  data: {
+    db: undefined,
+    loading: true,
+    displayPokemon: false,
+    searchPokemon: "",
+    searchResults: [],
+    selectedPokemon: "",
+    displayPokemon: false
+  },
+
+  watch: {
+    searchPokemon: function(pokemonName) {
+      pokemonName = _.trim(pokemonName);
+
+      this.suggestPokemons(pokemonName);
+    }
+  },
+
+  methods: {
+    suggestPokemons: function(pokemonName) {
+      if (pokemonName === "") {
+        this.searchResults = [];
+        return;
+      }
+
+      this.searchResults = _.filter(this.db, o => o.identifier.startsWith(pokemonName));
+    },
+
+    choosePokemon: function(pokemon) {
+      this.searchPokemon = pokemon.identifier;
+      this.selectedPokemon = pokemon;
+      this.displayPokemon = true;
+      console.log(this.selectedPokemon);
+    }
+  },
+
+  created: async function() {
+    const response = await axios.get("assets/db.json");
+
+    // After db download finished
+    this.db = response.data;
+    this.loading = false;
+  }
+});
